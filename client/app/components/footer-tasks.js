5a790e8a442bf7e046115111b829580352601a58
Ariel Fisher <fishondor@gmail.com>
2019-01-01 13:43:33 +0200

    Add computed properties: finished and unfinished

--- client/app/components/footer-tasks.js
+++ client/app/components/footer-tasks.js
@@ -1,5 +1,7 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
+import { filterBy } from '@ember/object/computed';
 
 export default Component.extend({
 
@@ -12,4 +14,14 @@ export default Component.extend({
     willRender(){
         this.set('STRINGS', this.get('constants').STRINGS);
     },
+
+    tasksFinished: computed('tasksList.@each.done', function(){
+        console.log("tasksList", this.tasksList);
+        return this.tasksList.filterBy( 'done', true).length;
+    }),
+    tasksUnfinished: computed('tasksList.@each.done', function(){
+        console.log("tasksList", this.tasksList);
+        return this.tasksList.filterBy( 'done', false).length;
+    }),
+
 });
