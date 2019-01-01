f1806de975a7f61c0870d2ccad3909d637fc365a
Ariel Fisher <fishondor@gmail.com>
2019-01-01 14:04:16 +0200

    Remove unused import

--- client/app/components/footer-tasks.js
+++ client/app/components/footer-tasks.js
@@ -1,7 +1,6 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 import { computed } from '@ember/object';
-import { filterBy } from '@ember/object/computed';
 
 export default Component.extend({
 
