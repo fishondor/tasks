e59f613eb5a9b979a841dc25f5521534d3dff524
Ariel Fisher <fishondor@gmail.com>
2019-01-01 15:58:39 +0200

    Remove console

--- client/app/controllers/tasks.js
+++ client/app/controllers/tasks.js
@@ -4,7 +4,6 @@ export default Controller.extend({
 
     actions: {
         addTaskItem(){
-            console.log("addTaskItem", this.get('model.length'));
             this.store.push({data : [{id: this.get('model.length') + 1, type: 'task', attributes: {name: "Test 3", done: false}}]});
         }
     }
