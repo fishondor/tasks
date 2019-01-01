d7aa9c069a098a0619be6c536afe7f6dffe7d6d2
Ariel Fisher <fishondor@gmail.com>
2019-01-01 16:23:53 +0200

    Remove unnecessary increment of id and add deleteItem action

--- client/app/controllers/tasks.js
+++ client/app/controllers/tasks.js
@@ -4,7 +4,14 @@ export default Controller.extend({
 
     actions: {
         addTaskItem(){
-            this.store.push({data : [{id: this.get('model.length') + 1, type: 'task', attributes: {name: "Test 3", done: false}}]});
+            this.store.push({data : [{id: this.get('model.length'), type: 'task', attributes: {name: "Test 3", done: false}}]});
+        },
+        deleteItem(id){
+            this.store.findRecord('task', id, { backgroundReload: false }).then(
+                task => {
+                    task.destroyRecord();
+                }
+            )
         }
     }
 });
