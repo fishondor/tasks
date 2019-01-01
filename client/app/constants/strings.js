dc92d39f61f0e452398d1cc4d6043393c6090f6b
Ariel Fisher <fishondor@gmail.com>
2019-01-01 12:34:46 +0200

    Add constants service and constants folder

---
+++ client/app/constants/strings.js
@@ -0,0 +1,11 @@
+const TASKS_TITLE = 'משימות';
+const TASKS_UNFINISHED = 'לסיום';
+const TASKS_FINISHED = 'הושלמו';
+const TASKS_TOTAL = 'סה"כ';
+
+export {
+    TASKS_TITLE,
+    TASKS_UNFINISHED,
+    TASKS_FINISHED,
+    TASKS_TOTAL
+}
