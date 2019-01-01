996c2ef27480dc6cae0e174d1f7f7236981b7bb6
Ariel Fisher <fishondor@gmail.com>
2019-01-01 12:34:12 +0200

    Add task-item component

---
+++ client/app/components/task-item.js
@@ -0,0 +1,14 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+
+    tagName: "li",
+    classNameBindings: ['task.done:done'],
+    classNames: ['task-item'],
+    actions:{
+        toggleDoneProperty(){
+            this.set("task.done", !this.get("task.done"));
+        }
+    }
+
+});
