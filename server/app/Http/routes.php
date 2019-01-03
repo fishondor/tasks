534da32bb6f1aa4ec2ec66fa2060538831ee6eaa
Ariel Fisher <ariel.fisher@zeek.me>
2019-01-03 13:20:23 +0200

    Set api routes to use resource controller

--- server/app/Http/routes.php
+++ server/app/Http/routes.php
@@ -1,16 +1,13 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+use App\Task;
+use Illuminate\Http\Request;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(array('prefix' => 'api'), function()
+{
+    Route::resource('tasks', 'TaskController');
 });
+
+Route::get('{ember?}', function() {
+    return View::make('ember');
+})->where('ember', '.*');
