9be93a8fac224677e15e8bf857332620676d3fe4
Ariel Fisher <fishondor@gmail.com>
2019-01-03 14:19:10 +0200

    Add JSON handling methods in task model file

--- client/app/models/task.js
+++ client/app/models/task.js
@@ -4,3 +4,33 @@ export default DS.Model.extend({
     name: DS.attr('string'),
     done: DS.attr('boolean')
 });
+
+var fromJSONObjectFormat = (model) => {
+    return {
+        name: model.data.attributes.name,
+        done: model.data.attributes.done
+    }
+}
+
+var toJSONObjectFormat = item => {
+    return {
+        type: 'task',
+        id: item.id,
+        attributes: {
+            name: item.name,
+            done: item.done == 'true' || item.done == 1
+        }
+    }
+}
+
+var toJSONArrayFormat = payload => {
+    return payload.map(
+        item => toJSONObjectFormat(item)
+    )
+}
+
+export {
+    fromJSONObjectFormat,
+    toJSONObjectFormat,
+    toJSONArrayFormat
+}
