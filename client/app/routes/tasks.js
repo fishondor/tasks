f87ad9bc76c064e3fdba84830f3da26e9bcf5756
Ariel Fisher <fishondor@gmail.com>
2019-01-04 14:59:32 +0200

    Add error page

--- client/app/routes/tasks.js
+++ client/app/routes/tasks.js
@@ -3,5 +3,10 @@ import Route from '@ember/routing/route';
 export default Route.extend({
     model(){
         return this.store.findAll('task');
+    },
+    actions: {
+        error() {
+            return true;
+        }
     }
 });
